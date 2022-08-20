6944c90de634c196b2451315bb78df75d7c70943
ismayilzeynal <ismayilbz@code.edu.az>
2022-08-21 03:00:10 +0400

    initial commit

---
+++ Pistol/Pistol/Models/Weapon.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;                             // Sleep() üçün 
+
+namespace Pistol.Models
+{
+    class Weapon
+    {
+        //ctor
+        public Weapon(int MagazineCapacity, int LeftBulletCount, float DischargeTime, string FireMode)
+        {
+            this.MagazineCapacity = MagazineCapacity;
+            this.LeftBulletCount = LeftBulletCount;
+            this.DischargeTime = DischargeTime;
+            this.FireMode = FireMode;
+        }
+
+
+        //fields
+        private int _magazineCapacity;
+        private int _leftBulletCount;
+        private float _dischargeTime;
+        private string _fireMode;
+
+        // props
+        public int MagazineCapacity 
+        {
+            get
+            {
+                return _magazineCapacity;
+            } 
+            set
+            {
+                if (value >= 0)
+                    _magazineCapacity = value;
+            }
+        }
+
+        public int LeftBulletCount
+        {
+            get
+            {
+                return _leftBulletCount;
+            }
+            set
+            {
+                if (value >= 0 && value <= MagazineCapacity)
+                    _leftBulletCount = value;
+            }
+        }
+
+        public float DischargeTime 
+        {
+            get
+            {
+                return _dischargeTime;
+            }
+            set
+            {
+                if (value > 0)
+                    _dischargeTime = value;
+            }
+        }
+
+        public string FireMode 
+        {
+            get
+            {
+                return _fireMode;
+            }
+            set
+            {
+                if (value == "Single" || value == "Auto")
+                    _fireMode = value;
+            } 
+        }
+
+        // methods
+        public void Shoot()
+        {
+
+            if (LeftBulletCount > 0)
+            {
+                LeftBulletCount--;
+                Console.WriteLine("a shot fired");
+
+                if (LeftBulletCount == 0)
+                    Console.WriteLine("Bullets over");
+            }
+            else
+                Reload();
+        }
+
+        public void Fire()
+        {
+
+            if (LeftBulletCount > 0)
+            {
+                float time = DischargeTime / MagazineCapacity * LeftBulletCount;
+                Thread.Sleep((int)time*1000);
+                Console.WriteLine($"{time} sec");
+                LeftBulletCount = 0;
+            }
+            else
+                Reload();
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return MagazineCapacity - LeftBulletCount;
+        }
+
+        public void Reload()
+        {
+            if(LeftBulletCount==MagazineCapacity)
+            {
+                Console.WriteLine("You can't reload");
+                return;
+            }
+            Console.WriteLine("Reloading...");
+            Thread.Sleep(3000);
+            LeftBulletCount = MagazineCapacity;
+            Console.WriteLine("Loaded");
+        }
+
+        public void ChangeFireMode()
+        {
+            if(MagazineCapacity==0)
+                Console.WriteLine("You can't change fire mode");
+            else
+            {
+                if (FireMode == "Single")
+                    FireMode = "Auto";
+                else
+                    FireMode = "Single";
+
+                Console.WriteLine($"Fire mode: {FireMode}");
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Magazine capacity: {MagazineCapacity} \t Left bullet count: {LeftBulletCount} \t Discharge time: {DischargeTime} \t Fire mode: {FireMode}");
+        }
+
+
+
+
+
+
+
+        public void ChangeMagCap()
+        {
+            Console.WriteLine($"Currently magazine capacity: {this.MagazineCapacity}");
+            Console.Write("New capacity: ");
+
+            TopMag:
+                bool Test = true;
+                int MagCap = -1;
+                while (Test)
+                {
+                    Program.InputArrows();
+                    Test = !int.TryParse(Console.ReadLine(), out MagCap);
+                    if (Test || MagCap < 0)
+                        Program.PEC(ref Test);
+                }
+
+                if(MagCap==MagazineCapacity)
+                {
+                    Console.WriteLine("New and old can't be same. Please re enter");
+                    goto TopMag;
+                }
+
+                MagazineCapacity = MagCap;
+                if (LeftBulletCount > MagazineCapacity)
+                    LeftBulletCount = MagazineCapacity;
+        }
+
+
+        public void ChangeBulletCount()
+        {
+            Console.WriteLine($"Currently bullet count: {this.LeftBulletCount}");
+            Console.Write("New count: ");
+
+            TopBullet:
+                bool Test = true;
+                int LeftBullet = -1;
+                while (Test)
+                {
+                    Program.InputArrows();
+                    Test = !int.TryParse(Console.ReadLine(), out LeftBullet);
+                    if (Test || LeftBullet < 0 || LeftBullet > MagazineCapacity)
+                        Program.PEC(ref Test);
+                }
+
+                if (LeftBullet == LeftBulletCount)
+                {
+                    Console.WriteLine("New and old can't be same. Please re enter");
+                    goto TopBullet;
+                }
+
+                LeftBulletCount = LeftBullet;
+        }
+
+
+        public void ChangeDischargeTime()
+        {
+            Console.WriteLine($"Currently discharge time: {this.DischargeTime}");
+            Console.Write("New discharge time: ");
+
+            TopDischarge:
+            bool Test = true;
+            float DisChTime = -1;
+            while (Test)
+            {
+                Program.InputArrows();
+                Test = !Single.TryParse(Console.ReadLine(), out DisChTime);
+                if (Test || DisChTime < 0)
+                    Program.PEC(ref Test);
+            }
+
+            if (DisChTime == DischargeTime)
+            {
+                Console.WriteLine("New and old can't be same. Please re enter");
+                goto TopDischarge;
+            }
+
+            DischargeTime = DisChTime;
+        }
+
+
+
+    }
+}
---
+++ Pistol/Pistol/Program.cs
@@ -0,0 +1,272 @@
+﻿using Pistol.Models;
+using System;
+
+namespace Pistol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Main:
+                Console.Clear();
+                MainMenu();
+
+                //Console.WriteLine("0) exit \n1) Main");
+                //int choise = -1;
+                //while (choise != 0 && choise != 1)
+                //    TryInt(out choise);
+                //if (choise == 0)
+                //    Exit();
+                //else
+                //    goto Main;
+        }
+
+
+
+
+
+        public static void MainMenu()
+        {
+            int choise=-1;
+            Console.WriteLine("1) Create custom weapon  \n2) Choose advance weapon  \n0) Exit");
+            InputArrows();
+            while (choise < 0 || choise > 2)
+                TryInt(out choise);
+
+            if (choise == 1)
+                CustomWeapon();
+            else if (choise == 0)
+                Exit();
+            
+                
+        }
+
+
+
+
+
+
+
+
+
+        public static void CustomWeapon()
+        {
+            int MagazineCapacity = -1, LeftBulletCount = -1, FireMode = -1;
+            float DischargeTime = -1;
+            string FireModeName = "";
+            bool Test = true;           // (inverse) test for if input is false
+
+
+            //cls
+            Console.WriteLine("Please input fields \n");
+            while (Test)
+            {
+                Console.Write("Magazine capacity: ");
+                InputArrows();
+                Test = !int.TryParse(Console.ReadLine(), out MagazineCapacity);
+                if (Test || MagazineCapacity < 0)
+                    PEC(ref Test);
+            }
+            if (MagazineCapacity == 0 || MagazineCapacity == 1)
+            {
+                Console.WriteLine("Weapon only have a single fire mode");
+                FireModeName = "Single";
+                DischargeTime = 0;
+                MagazineCapacity = 1;
+                LeftBulletCount = 1;
+                goto Bottom;
+            }
+
+            Test = true;
+            while (Test)
+            {
+                Console.Write("Bullet count in magazine: ");
+                InputArrows();
+                Test = !int.TryParse(Console.ReadLine(), out LeftBulletCount);
+                if (Test || LeftBulletCount < 0 || LeftBulletCount > MagazineCapacity)
+                    PEC(ref Test);
+            }
+            
+
+            Test = true;
+            while (Test)
+            {
+                Console.Write("Discharge time: ");
+                InputArrows();
+                Test = !Single.TryParse(Console.ReadLine(), out DischargeTime);
+                if (Test || DischargeTime <= 0)
+                    PEC(ref Test);
+
+            }
+
+
+            Test = true;
+            while (Test)
+            {
+                Console.WriteLine("Choose fire mode:  \n1) Single  \n2) Auto");
+                InputArrows();
+                Test = !int.TryParse(Console.ReadLine(), out FireMode);
+                if (Test || FireMode < 1 || FireMode > 2)
+                    PEC(ref Test);
+                else
+                {
+                    if (FireMode == 1)
+                        FireModeName = "Single";
+                    else
+                        FireModeName = "Auto";
+                }
+            }
+
+            Bottom:
+                Weapon Gun = new Weapon(MagazineCapacity, LeftBulletCount, DischargeTime, FireModeName);
+                CommandMenu(Gun);
+        }
+
+
+
+
+
+
+
+
+        public static void CommandMenu(Weapon Gun)
+        {
+            Commands:
+                Console.Clear();
+                int choise = -1;
+                Console.WriteLine("0) Info \n1) Shoot \n2) Fire \n3) Get Remain Bullet Count \n4) Reload \n5) Change Fire Mode \n6) Exit \n7) Edit");
+                while (!(choise >= 0 && choise < 8))
+                    TryInt(out choise);
+
+                Console.Clear();
+                switch(choise)
+                {
+                    case 0:
+                        Gun.GetInfo();
+                        break;
+                    case 1:
+                        Gun.Shoot();
+                        break;
+                    case 2:
+                        Gun.Fire();
+                        break;
+                    case 3:
+                    Console.WriteLine(Gun.GetRemainBulletCount()); 
+                        break;
+                    case 4:
+                        Gun.Reload();
+                        break;
+                    case 5:
+                        Gun.ChangeFireMode();
+                        break;
+                    case 6:
+                        Exit();
+                        return;
+                        break;
+                    case 7:
+                        Edit(Gun);
+                        break;
+                    default:
+                        Console.WriteLine("Error 404");
+                        break;
+                }
+
+                choise = -1;
+                Console.WriteLine("0) Back  \n1) Exit");
+                while (choise != 0 && choise != 1)
+                    TryInt(out choise);
+                if (choise == 0)
+                    goto Commands;
+                else
+                    Exit();
+
+
+        }
+
+
+
+
+
+
+        public static void Edit(Weapon Gun)
+        {
+            char choise = ' ';
+            Console.WriteLine("T) Magazine capacity \nS) Bullets count \nD) Discharge time");
+            while (choise != 'T' && choise != 'S' && choise != 'D')
+                TryChar(out choise);
+
+
+            switch (choise)
+            {
+                case 'T':
+                    Gun.ChangeMagCap();
+                    break;
+                case 'S':
+                    Gun.ChangeBulletCount();
+                    break;
+                case 'D':
+                    Gun.ChangeDischargeTime();
+                    break;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // little help methods
+
+        public static void PEC(ref bool test)           // please enter correctly
+        {
+            Console.WriteLine("Please enter correctly");
+            test = true;
+        }
+
+
+        public static void InputArrows()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(">>> ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+
+        public static void TryInt(out int choise)
+        {
+
+            while (!int.TryParse(Console.ReadLine(), out choise))
+            {
+                InputArrows();
+                Console.WriteLine("Please enter correctly");
+            }
+        }
+
+        public static void TryChar(out char choise)
+        {
+            while (!Char.TryParse(Console.ReadLine(), out choise))
+            {
+                Console.WriteLine("Please enter correctly");
+            }
+            choise = Char.ToUpper(choise);
+        }
+
+
+        public static void Exit()
+        {
+            Console.WriteLine("Thanks for using");
+        }
+
+
+    }
+}
